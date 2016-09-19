d2629bed67cf12cf177fe89f87ed0237aabb0dc9
Unknown <nicolas.vess@gmail.com>
2016-09-19 18:35:18 +0200

    Update my folders

--- template/_header.php
+++ template/_header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>A2-PREPRO</title>
-
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript" src="js/nav.min.js" ></script>
@@ -11,8 +10,22 @@
 <body>
 
 <header>
+
+
     <a class="mobile" href="#">&#9776;</a>
     <nav>
+        <div id="bars">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
         <ul>
             <li><a href="#">Classique</a></li>
             <li><a href="#">Rap</a></li>
