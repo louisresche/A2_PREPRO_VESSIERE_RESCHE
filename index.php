cbd7a0c8194c88a29ff225b6708a80f21fd87ba4
louisresche <louis.resche@gmail.com>
2016-09-19 16:40:53 +0200

    add reset.css

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <title>A2-PREPRO</title>
 
-    
+    <link rel="stylesheet" type="text/css" href="style/reset.css">
+    <link rel="stylesheet" type="text/css" href="style/main.css">
 </head>
 <body>
 
