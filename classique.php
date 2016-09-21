f2662dda1645e247341758d94f7968dedd30db24
Unknown <nicolas.vess@gmail.com>
2016-09-21 16:41:22 +0200

    Modified text in "classique" page

--- classique.php
+++ classique.php
@@ -10,7 +10,7 @@
             <div class="classique-text">
 
                    <p> La musique classique désigne habituellement l'ensemble de la musique occidentale savante d'origine liturgique et séculière, par opposition à la musique populaire, depuis la musique médiévale à nos jours.
-                          L'adjectif classique ne se réfère stricto sensu qu'à la musique de la période classique écrite entre le milieu du xviiie siècle et l'avènement de la musique romantique dans les années 18201.</p>
+                          L'adjectif classique ne se réfère stricto sensu qu'à la musique de la période classique écrite entre le milieu du XVIIIe siècle et l'avènement de la musique romantique dans les années 1820.</p>
 
                 </div>
 
