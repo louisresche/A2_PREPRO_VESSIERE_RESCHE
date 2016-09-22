106d8604c91984324584cbfd712cf025b145b3ce
louisresche <louis.resche@gmail.com>
2016-09-23 01:13:08 +0200

    little fix in "techno". image + scss

--- techno.php
+++ techno.php
@@ -1,14 +1,18 @@
 <?php include "template/_header.php"; ?>
 
-<div class="rap-block">
+<div class="techno-block">
 
-    <div class="rap-text">
+    <div class="techno-text">
         <p> La techno est un genre de musique électronique ayant émergé aux États-Unis au milieu des années 19802
-            Le plus souvent composée en home studio et réinterprétée par des disc jockeys lors de pratiques festives, la techno est avant tout une musique de danse, par essence répétitive.</p>
+            Le plus souvent composée en home studio et réinterprétée par des disc jockeys lors de pratiques festives,
+            la techno est avant tout une musique de danse, par essence répétitive.</p>
         <div class="miniatures">
-            <img src="images/faki.jpeg" alt="Mozart"> </img>
-            <img src="images/kerri.jpeg" alt="Beethoven"> </img>
+            <img src="images/faki.jpeg" alt="faki">
+            <img src="images/kerri.jpeg" alt="kerri">
         </div>
     </div>
+</div>
+<div class="techno-img">
+    <h1 class="titre">TECHNO</h1>
 </div>
 
