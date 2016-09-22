31e099600ff9c6dde93b305b9878d42ecabfdb39
Unknown <nicolas.vess@gmail.com>
2016-09-22 16:35:21 +0200

    Added black bloc and text in "Electro" page

--- electro.php
+++ electro.php
@@ -2,5 +2,15 @@
 
 
 
+    <div class="classique-block">
+
+        <div class="classique-text">
+            <p>  La musique électronique est un type de musique conçu dans les années 1950 avec des générateurs de signaux et de sons synthétiques.
+                Avant de pouvoir être utilisée en temps réel, elle a été primitivement enregistrée sur bande magnétique, ce qui permettait aux compositeurs de manier aisément les sons, par exemple dans l'utilisation de boucles répétitives superposées.
+                Ses précurseurs ont pu bénéficier de studios spécialement équipés ou faisaient partie d'institutions musicales pré-existantes.
+            </p>
+
+        </div>
+   
 
 <?php include  "template/_footer.php"; ?>
