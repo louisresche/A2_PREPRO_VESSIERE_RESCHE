2cc614dbf1d5c028ccb7022aefdde7c23926a430
louisresche <louis.resche@gmail.com>
2016-09-23 01:11:45 +0200

    little fix in "electro". image + scss

--- electro.php
+++ electro.php
@@ -2,11 +2,11 @@
 
 
     <div class="electro-img">
-        <h1 class="titre">Electro</h1>
+        <h1 class="titre">ELECTRO</h1>
     </div>
 
-    <div class="classique-block">
-        <div class="classique-text">
+    <div class="electro-block">
+        <div class="electro-text">
             <p>  La musique électronique est un type de musique conçu dans les années 1950 avec des générateurs de signaux et de sons synthétiques.
                 Avant de pouvoir être utilisée en temps réel, elle a été primitivement enregistrée sur bande magnétique, ce qui permettait aux compositeurs de manier aisément les sons, par exemple dans l'utilisation de boucles répétitives superposées.
                 Ses précurseurs ont pu bénéficier de studios spécialement équipés ou faisaient partie d'institutions musicales pré-existantes.
