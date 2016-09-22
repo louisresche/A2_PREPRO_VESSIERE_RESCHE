b119b2bd2ba8d6eb744edf6221b8b9f74e818062
Unknown <nicolas.vess@gmail.com>
2016-09-22 17:44:07 +0200

    Added black bloc and text in "Techno" page

--- techno.php
+++ techno.php
@@ -1,1 +1,8 @@
 <?php include "template/_header.php"; ?>
+
+<div class="rap-block">
+
+    <div class="rap-text">
+        <p> La techno est un genre de musique électronique ayant émergé aux États-Unis au milieu des années 19802
+            Le plus souvent composée en home studio et réinterprétée par des disc jockeys lors de pratiques festives, la techno est avant tout une musique de danse, par essence répétitive.</p>
+</div>
