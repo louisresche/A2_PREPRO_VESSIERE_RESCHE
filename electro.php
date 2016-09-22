2f0f4383c342859f6859d1a5889abb4b83095eae
Unknown <nicolas.vess@gmail.com>
2016-09-22 17:27:36 +0200

    Modified images in "Electro" page + Ajusted SCSS

--- electro.php
+++ electro.php
@@ -9,8 +9,14 @@
                 Avant de pouvoir être utilisée en temps réel, elle a été primitivement enregistrée sur bande magnétique, ce qui permettait aux compositeurs de manier aisément les sons, par exemple dans l'utilisation de boucles répétitives superposées.
                 Ses précurseurs ont pu bénéficier de studios spécialement équipés ou faisaient partie d'institutions musicales pré-existantes.
             </p>
-
+            <div class="miniatures">
+                <img src="images/noize.jpeg" alt="Mozart"> </img>
+                <img src="images/breakbot.jpeg" alt="Beethoven"> </img>
+            </div>
         </div>
-   
+    </div>
+    <div class="classique-img">
+        <h1 class="titre">Electro</h1>
+    </div>
 
 <?php include  "template/_footer.php"; ?>
