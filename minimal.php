b14f158ed49d8916bc4add3d4f34502dd32d1ff4
Unknown <nicolas.vess@gmail.com>
2016-09-23 16:17:31 +0200

    Added text + images in "Minimal" page + SCSS

---
+++ minimal.php
@@ -0,0 +1,22 @@
+<?php include "template/_header.php"; ?>
+
+
+
+
+
+
+
+<div class="classique-block">
+
+    <div class="classique-text">
+        <p> La techno minimale est un sous-genre minimal de la techno, caractérisée par une structure musicale répétitive.
+            La minimale semble s'être développée au début des années 1990 et lancée par les producteurs originaires de Détroit Robert Hood et Daniel Bell.
+            Au début des années 2000, le terme « minimale » décrit à l'origine un style de techno popularisé en Allemagne par des labels tels que Kompakt, Perlon, et M-nus de Richie Hawtin.
+        </p>
+        <div class="miniatures">
+            <img src="images/carl.jpg" alt="Mozart"> </img>
+            <img src="images/hood.jpg" alt="Beethoven"> </img>
+        </div>
+    </div>
+
+</div>
