c1ebfd106117dc0b246002c9d78cd0a72dab4439
Unknown <nicolas.vess@gmail.com>
2016-09-23 15:55:05 +0200

    Added background image in "Dubstep" + H1

--- dubstep.php
+++ dubstep.php
@@ -3,6 +3,8 @@
 
 <div class="dubstep-block">
 
+
+
     <div class="dubstep-text">
         <p> La dubstep est un genre de musique électronique, originaire pour sa plus grande partie du Sud de Londres.
             Il émerge à la fin des années 1990 et se développe grâce à de nombreux styles musicaux connexes tels que le 2-step garage, broken beat, drum and bass, jungle, dub et reggae.
@@ -13,9 +15,8 @@
             <img src="images/funtcase.jpeg" alt="Beethoven"> </img>
         </div>
     </div>
+</div>
 
-
-
-
-
+<div class="dubstep-img">
+    <h1 class="titre">Dubstep</h1>
 </div>
