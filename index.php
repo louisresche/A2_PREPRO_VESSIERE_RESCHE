2a295927267175080ac382696d04bc8388bf8bb9
louisresche <louis.resche@gmail.com>
2016-09-23 18:07:58 +0200

    switch h1 to p

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php include "template/_header.php"; ?>
 
 
-    <h1 class="titre-home">La musique est l'art consistant à combiner <br>
+    <p class="titre-home">La musique est l'art consistant à combiner <br>
         sons et silences au cours du temps : <br>
         le rythme est le support de cette <br>
         combinaison dans le temps, la hauteur, <br>
         celle de la combinaison <br>
-        dans les fréquences.</h1>
+        dans les fréquences.</p>
 <video poster="" id="bgvid" playsinline autoplay muted loop>
     <source src="images/vinyl.mp4" type="video/mp4">
 
